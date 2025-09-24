9a5eb20343c1fcc760bbee1f998ae83138d4d5fa
nirananvan <isasaki@dl-techx-xedge.net>
2025-09-24 13:26:34 +0900

    資源のデプロイ

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app color="light-green">
+
+
+      <v-toolbar-title class="white--text">
+        きょうのやること
+      </v-toolbar-title>
+     
+      <v-spacer></v-spacer>
+
+      <v-btn  text to="/" class="white--text">ついか</v-btn>
+      <v-btn  text to="/kids" class="white--text">いちらん</v-btn>
+    
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
